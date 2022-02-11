d7f52bca5c13dfc621ca2f4731df260b02ac12bc
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2022-02-11 10:13:55 +0100

    update symlinks

--- initsymlinks.sh
+++ initsymlinks.sh
@@ -1,1 +1,20 @@
-ln -f FlappyBlock/* docs/
+FOLDER=FlappyBlock/*
+OUTPUT_DIR=docs/
+
+for filename in $OUTPUT_DIR*;
+do
+  if [ $filename != "${OUTPUT_DIR}CNAME" ];
+  then
+    rm -f $filename;
+    echo "removed $filename";
+  fi;
+done
+
+for filename in $FOLDER*;
+do
+  if [ ${filename##*/} != "README.md" ];
+    then
+      ln -f $filename $OUTPUT_DIR;
+      echo "linked $filename";
+  fi;
+done
